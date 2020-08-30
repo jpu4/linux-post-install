63f7ff6dda8deabd805cb1e7d6a9170feab0416c
James Ussery <33546073+jpu4@users.noreply.github.com>
2020-08-30 18:24:31 +0530

    updated third party urls

--- solus-budgie/post-install.sh
+++ solus-budgie/post-install.sh
@@ -2,166 +2,161 @@
 
 # Author: James Ussery <James@Ussery.me>
 # Date Created: 20180314
+# Date Updated: 20200830
 # Description: Solus Budgie Post install.
 # As always, read through each item use the hash symbol "#" to stop a package from installing.
 # I've used individual eopkg entries because I've found in the past apps get skipped if there's an error.
 #
 # References: 
 #
-# Solus Third Party page: https://solus-project.com/articles/software/third-party/en/
+# Solus Third Party page: https://getsol.us/articles/software/third-party/en/
 #-------------------------------------------------------------------------------------
 
 dlpath="~/Downloads/"
 
 echo "Remove programs I don't like"
-eopkg -y remove amarok dragon
+sudo eopkg -y remove amarok dragon
 
 echo "Upgrade system"
-eopkg -y upgrade
+sudo eopkg -y upgrade
 
 echo "== GAMING =="
-# eopkg -y install steam
+# sudo eopkg -y install steam
 
 echo "== MEDIA =="
-eopkg -y install vlc
-eopkg -y install kodi
-eopkg -y install youtube-dl
-eopkg -y install youtube-dl-gui
-
-# SPOTIFY
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/spotify/pspec.xml
-eopkg it spotify*.eopkg;rm spotify*.eopkg
+sudo eopkg -y install vlc
+sudo eopkg -y install kodi
+sudo eopkg -y install youtube-dl
+sudo eopkg -y install youtube-dl-gui
 
 echo "== CHAT =="
-eopkg -y install pidgin
-eopkg -y install signal-desktop
-
-# SKYPE
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/skype/pspec.xml
-eopkg it skype*.eopkg;rm *.eopkg
-
-# SLACK
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/slack-desktop/pspec.xml
-eopkg it slack-desktop*.eopkg;rm slack-desktop*.eopkg
+sudo eopkg -y install discord
+sudo eopkg -y install signal-desktop
 
 echo "== BROWSERS =="
-# CHROME
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
-eopkg it google-chrome-*.eopkg;rm google-chrome-*.eopkg
-
 # VIVALDI
-eopkg -y install vivaldi-stable
-# VIVALDI FLASH
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/flash-player-ppapi/pspec.xml
-eopkg it flash-player-ppapi*.eopkg;rm flash-player-ppapi*.eopkg
+sudo eopkg -y install vivaldi-stable
 
 echo "== PRODUCTIVITY =="
-eopkg -y install rednotebook
-eopkg -y install focuswriter
-eopkg -y install thunderbird
-eopkg -y install libreoffice
-
-# WPS OFFICE
-# Linux replica of MS Office
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/wps-office/pspec.xml
-eopkg it wps-office*.eopkg;rm wps-office*.eopkg
-
-echo "== GRAPHICS =="
-eopkg -y install shotwell
-eopkg -y install rawstudio
-eopkg -y install gimp
-
-echo "== SECURITY =="
-eopkg -y install keepass
-eopkg -y install tor
-systemctl enable tor
-systemctl start tor
+sudo eopkg -y install focuswriter
+sudo eopkg -y install thunderbird
+sudo eopkg -y install libreoffice
 
 echo "== UTILITIES =="
-eopkg -y install guake
-eopkg -y install gnome-disk-utility
-eopkg -y install gnome-tweak-tool
-eopkg -y install krename
-eopkg -y install remmina
-eopkg -y install owncloud-client
-eopkg -y install solaar
-eopkg -y install speedtest-cli
-eopkg -y install rsync
-eopkg -y install s3cmd
-eopkg -y install unrar
-eopkg -y install unzip
-eopkg -y install wget
-eopkg -y install htop
+sudo eopkg -y install guake
+sudo eopkg -y install gnome-disk-utility
+sudo eopkg -y install gnome-tweak-tool
+sudo eopkg -y install krename
+sudo eopkg -y install remmina
+sudo eopkg -y install solaar
+sudo eopkg -y install speedtest-cli
+sudo eopkg -y install rsync
+sudo eopkg -y install s3cmd
+sudo eopkg -y install unrar
+sudo eopkg -y install unzip
+sudo eopkg -y install wget
+sudo eopkg -y install htop
 
 echo "== DEVELOPMENT =="
-eopkg -y install filezilla
-eopkg -y install subversion
-eopkg -y install git
-eopkg -y install atom
-eopkg -y install vscode
-
-# GITKRAKEN
-eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/gitkraken/pspec.xml
-eopkg it gitkraken*.eopkg;rm gitkraken*.eopkg
+sudo eopkg -y install filezilla
+sudo eopkg -y install git
+sudo eopkg -y install vscode
 
 echo "== Database =="
 # = MYSQL =
-eopkg -y install mariadb mariadb-server
+sudo eopkg -y install mariadb mariadb-server
 systemctl enable mariadb
 systemctl start mariadb
-eopkg -y install dbeaver
+sudo eopkg -y install dbeaver
 
 # Secure mariadb
 mysql_secure_installation
 
 # = MongoDB =
-eopkg -y install mongodb mongodb-tools
+sudo eopkg -y install mongodb mongodb-tools
 systemctl start mongod
 
 # = Node.js =
-eopkg -y install nodejs npm
+sudo eopkg -y install nodejs npm
 
 # = Apache (httpd+php) =
-eopkg -y install httpd
-eopkg -y install php
-eopkg -y install php-mysqlnd
-eopkg -y install php-opcache
-eopkg -y install php-pecl-apcu
-eopkg -y install php-cli
-eopkg -y install php-pear
-eopkg -y install php-pdo
-eopkg -y install php-mysqlnd
-eopkg -y install php-pgsql
-eopkg -y install php-pecl-mongodb
-eopkg -y install php-pecl-memcache
-eopkg -y install php-pecl-memcached
-eopkg -y install php-gd
-eopkg -y install php-mbstring
-eopkg -y install php-mcrypt
-eopkg -y install php-xml
-systemctl enable httpd
-systemctl start httpd
+sudo eopkg -y install nginx
+sudo eopkg -y install php
+sudo eopkg -y install php-mysqlnd
+sudo eopkg -y install php-opcache
+sudo eopkg -y install php-pecl-apcu
+sudo eopkg -y install php-cli
+sudo eopkg -y install php-pear
+sudo eopkg -y install php-pdo
+sudo eopkg -y install php-mysqlnd
+sudo eopkg -y install php-pgsql
+sudo eopkg -y install php-pecl-mongodb
+sudo eopkg -y install php-pecl-memcache
+sudo eopkg -y install php-pecl-memcached
+sudo eopkg -y install php-gd
+sudo eopkg -y install php-mbstring
+sudo eopkg -y install php-mcrypt
+sudo eopkg -y install php-xml
+systemctl enable nginx
+systemctl start nginx
 
-eopkg -y install pip
+sudo eopkg -y install pip
 pip install --upgrade pip
 
-eopkg -y install virtualbox
-eopkg -y install virtualbox-common
-eopkg -y install virtualbox-current
+sudo eopkg -y install virtualbox
+sudo eopkg -y install virtualbox-common
+sudo eopkg -y install virtualbox-current
+
+
+echo "== THIRD PARTY SOFTWARE =="
+
+# SPOTIFY
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/multimedia/music/spotify/pspec.xml
+sudo eopkg it spotify*.eopkg;sudo rm spotify*.eopkg
+
+# SKYPE
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/im/skype/pspec.xml
+sudo eopkg it skype*.eopkg;sudo rm *.eopkg
+
+# SLACK
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/im/slack-desktop/pspec.xml
+sudo eopkg it slack-desktop*.eopkg;sudo rm slack-desktop*.eopkg
+
+# CHROME
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/web/browser/google-chrome-stable/pspec.xml
+sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+
+# GOOGLE TALK
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/im/google-talkplugin/pspec.xml
+sudo eopkg it google-talkplugin*.eopkg;sudo rm google-talkplugin*.eopkg
+
+# VIVALDI FLASH
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/flash-player-ppapi/pspec.xml
+sudo eopkg it flash-player-ppapi*. eopkg;rm flash-player-ppapi*. eopkg
+
+# TEAMVIEWER
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/network/util/teamviewer/pspec.xml
+sudo eopkg it teamviewer*.eopkg;sudo rm teamviewer*.eopkg
+sudo systemctl start teamviewerd.service
+
+# MICROSOFT CORE FONTS
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/desktop/font/mscorefonts/pspec.xml
+sudo eopkg it mscorefonts*.eopkg;sudo rm mscorefonts*.eopkg
+
+# GITKRAKEN
+sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/getsolus/3rd-party/master/programming/gitkraken/pspec.xml
+sudo eopkg it gitkraken*.eopkg;sudo rm gitkraken*.eopkg
 
 
 echo "== SETTINGS =="
 # Add global git values
-git config --global user.name "First Last"
-git config --global user.email "your@email.com"
-
-# Autostart utilities
-cp autostart/guake.desktop ~/.config/autostart
-cp autostart/ownCloud.desktop ~/.config/autostart
+git config --global user.name "James Ussery"
+git config --global user.email "james@ussery.me"
 
 
-eopkg upgrade -y
-eopkg clean packages -y
+sudo eopkg upgrade -y
+sudo eopkg clean packages -y
 
 #REBOOT
 shutdown now -r
+
