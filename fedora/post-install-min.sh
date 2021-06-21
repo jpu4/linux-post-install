1ca70cc09b5ffd78adc8b601b42d301f0588293f
James Ussery <33546073+jpu4@users.noreply.github.com>
2021-06-21 09:50:42 +0530

    anydesk - pangox dependency

--- fedora/post-install-min.sh
+++ fedora/post-install-min.sh
@@ -116,6 +116,18 @@ echo "== Security =="
 echo "=== Bitwarden ==="
 sudo snap install bitwarden
 
+# anydesk
+cat > /etc/yum.repos.d/AnyDesk-Fedora.repo << "EOF" 
+[anydesk]
+name=AnyDesk Fedora - stable
+baseurl=http://rpm.anydesk.com/fedora/$basearch/
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY
+EOF
+sudo dnf install -y http://springdale.princeton.edu/data/springdale/7/x86_64/os/Addons/Packages/pangox-compat-0.0.2-2.sdl7.x86_64.rpm
+sudo dnf install -y anydesk
+
 sudo dnf upgrade -y
 sudo dnf clean packages -y
 reboot
