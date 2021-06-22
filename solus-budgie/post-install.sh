2b5c5a0ee8449f5f2bb250a3306b177d4740363f
James Ussery <33546073+jpu4@users.noreply.github.com>
2021-06-22 10:40:57 +0530

    snap install bitwarden

--- solus-budgie/post-install.sh
+++ solus-budgie/post-install.sh
@@ -73,6 +73,7 @@ sudo eopkg -y install fish
 sudo eopkg -y install flameshot
 sudo eopkg -y install nextcloud-client
 sudo eopkg -y install pencil
+sudo snap install bitwarden
 
 echo "== DEVELOPMENT =="
 sudo eopkg -y install filezilla
