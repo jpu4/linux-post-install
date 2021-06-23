1aaa05a3df5fda1ed68b05cf6905ac2979d08181
James Ussery <33546073+jpu4@users.noreply.github.com>
2021-06-23 08:17:54 +0530

    obs-studio

--- solus-budgie/post-install.sh
+++ solus-budgie/post-install.sh
@@ -71,6 +71,7 @@ sudo eopkg -y install vim
 sudo eopkg -y install curl
 sudo eopkg -y install fish
 sudo eopkg -y install flameshot
+sudo eopkg -y install obs-studio
 sudo eopkg -y install nextcloud-client
 sudo eopkg -y install pencil
 sudo snap install bitwarden
