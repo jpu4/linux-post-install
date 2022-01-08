c55cb8399dc56092637857995292a5061c78a876
James Ussery <James@Ussery.me>
2022-01-08 14:40:13 +0530

    aur snap and sed

--- arch/post-install.sh
+++ arch/post-install.sh
@@ -4,6 +4,7 @@ GIT_USER=""
 GIT_EMAIL=""
 CMD_INSTALL="sudo pacman -S --noconfirm "
 DIR_START="~/Downloads"
+DIR_WEBCONF="/localhost/config/conf"
 
 start () {
     sudo systemctl enable $1
@@ -12,6 +13,17 @@ start () {
 
 restart () {
     sudo systemctl restart $1
+}
+
+aur_install () {
+    git clone https://aur.archlinux.org/$1.git
+    cd $1
+    makepkg -si
+    cd $DIR_START
+}
+
+snap_install () {
+    sudo snap install $1
 }
 
 cd $DIR_START
@@ -24,6 +36,9 @@ $CMD_INSTALL git
 git config --global user.name="$GIT_USER"
 git config --global user.email="$GIT_EMAIL"
 
+# SNAPD
+aur_install snapd
+start snapd
 
 # LEMP
 $CMD_INSTALL nginx
@@ -31,17 +46,11 @@ $CMD_INSTALL mariadb
 sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 $CMD_INSTALL php php-fpm
 
-# Find and replace nginx.conf
-# location / {
-# root   /usr/share/nginx/html;
-# index  index.html index.htm index.php;
-# }
-# location ~ \.php$ {
-# fastcgi_pass   unix:/var/run/php-fpm/php-fpm.sock;
-# fastcgi_index  index.php;
-# root   /usr/share/nginx/html;
-# include        fastcgi.conf;
-# }
+sudo sed -i "s|#gzip  on;|include        $DIR_WEBCONF/*.conf;|g" /etc/nginx/nginx.conf
+# sed -i "s|index  index.html index.htm|index  index.html index.htm index.php;|g" /etc/nginx/nginx.conf
+# sed -i "s|include        fastcgi_params;|include        fastcgi.conf;|g" /etc/nginx/nginx.conf
+# sed -i "s|fastcgi_pass   127.0.0.1:9000;|fastcgi_pass   unix:/var/run/php-fpm/php-fpm.sock;|g" /etc/nginx/nginx.conf
+# sed -i "s|root           html;|root   /usr/share/nginx/html;|g" /etc/nginx/nginx.conf
 
 $CMD_INSTALL ufw
 sudo ufw allow 80/tcp
@@ -53,16 +62,10 @@ start nginx
 start php-fpm
 
 # SPOTIFY
-git clone https://aur.archlinux.org/spotify.git
-cd spotify
-makepkg -si
-cd $DIR_START
+snap_install spotify
 
 # VSCODE
-git clone https://aur.archlinux.org/visual-studio-code-bin.git
-cd visual-studio-code-bin
-makepkg -si
-cd $DIR_START
+aur_install visual-studio-code-bin
 
 # BEYOND COMPARE
 wget https://www.scootersoftware.com/bcompare-4.4.1.26165.x86_64.tar.gz
