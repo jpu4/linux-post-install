dc747ee900816d6278dcc403eeac959522a9fa9c
James Ussery <33546073+jpu4@users.noreply.github.com>
2022-02-12 20:18:32 +0530

    remi repo current version var

--- fedora/post-install.sh
+++ fedora/post-install.sh
@@ -36,7 +36,7 @@ sudo dnf -y upgrade
 echo "== REPOS =="
 sudo dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y https://rpms.remirepo.net/fedora/remi-release-35.rpm
+sudo dnf install -y https://rpms.remirepo.net/fedora/remi-release-$(rpm -E %fedora).rpm
 sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo curl -o /etc/yum.repos.d/skype-stable.repo https://repo.skype.com/rpm/stable/skype-stable.repo
 sudo dnf config-manager --add-repo http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
